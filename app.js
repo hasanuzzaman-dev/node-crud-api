08f516ee155013b90dffd3e9a7f42f97b6b2dc63
hasan-cpl <hasanuzzaman@cloudproductionltd.com>
2023-11-16 15:46:30 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+var cors = require('cors');
+const courseRouter = require('./routes/courseHandler');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+// send json body
+app.use(bodyParser.json());
+
+// creating corse route
+app.use("/api/v1", courseRouter);
+
+mongoose.connect(process.env.DB_CONNECTION_URL, () => {
+    console.log('Connected DB at ' + process.env.DB_CONNECTION_URL);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running at port', process.env.PORT);
+});
---
+++ routes/courseHandler.js
@@ -0,0 +1,93 @@
+const express = require('express');
+//const { route } = require('express/lib/application');
+const Course = require("../models/course");
+
+const router = express.Router();
+
+// Creating the routes
+
+/* router.get('/', (req, res) => {
+    res.send("Hello test");
+}); */
+
+// // Get all Courses
+router.get('/courses/', async (req, res) => {
+    try {
+        const courses = await Course.find();
+        res.json(courses);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// Get single couses
+router.get("/course/:courseId", async (req, res) => {
+
+    const courseId = req.params.courseId;
+    try {
+        const course = await Course.findById(courseId);
+        res.json(course);
+
+    } catch (err) {
+        res.json(err);
+    }
+}); 
+
+// Create course
+
+router.post("/course/", async (req, res) => {
+    try {
+       
+        const course = await Course.create(req.body);
+        res.json(course);
+    } catch (error) {
+        res.json(error)
+    }
+});
+
+// Delete Course
+router.delete("/course/:courseId", async (req, res) => {
+    try {
+        await Course.remove({
+            "_id": req.params.courseId
+        });
+        res.status(200).json({
+            "message": "Deleted successfully"
+        });
+    } catch (error) {
+        res.json({
+            "message": error
+        });
+    }
+});
+
+// Get by title
+
+router.get("/course/title/:title", async (req, res) => {
+
+    const title = req.params.title;
+    console.log(title);
+    try {
+        const course = await Course.find({title:title});
+        res.json(course);
+
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// Update course by id
+router.put("/:courseId", async(req, res) => {
+    const courseId = req.params.courseId;
+
+    try {
+        const course = await Course.updateOne({
+            '_id': courseId
+        }, req.body);
+    } catch (error) {
+        res.json(error);
+    }
+
+})
+
+module.exports = router;
