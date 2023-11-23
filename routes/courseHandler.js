0bf0eb460168307c899578b000b8e235a37066f6
hasan-cpl <hasanuzzaman@cloudproductionltd.com>
2023-11-23 19:56:47 +0600

    add docker-compose.yml

--- routes/courseHandler.js
+++ routes/courseHandler.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 //const { route } = require('express/lib/application');
 const Course = require("../models/course");
 
@@ -6,12 +6,12 @@ const router = express.Router();
 
 // Creating the routes
 
-/* router.get('/', (req, res) => {
-    res.send("Hello test");
-}); */
+router.get("/", (req, res) => {
+    res.send({ message: "API Running" });
+});
 
 // // Get all Courses
-router.get('/courses/', async (req, res) => {
+router.get("/courses/", async (req, res) => {
     try {
         const courses = await Course.find();
         res.json(courses);
@@ -22,26 +22,23 @@ router.get('/courses/', async (req, res) => {
 
 // Get single couses
 router.get("/course/:courseId", async (req, res) => {
-
     const courseId = req.params.courseId;
     try {
         const course = await Course.findById(courseId);
         res.json(course);
-
     } catch (err) {
         res.json(err);
     }
-}); 
+});
 
 // Create course
 
 router.post("/course/", async (req, res) => {
     try {
-       
         const course = await Course.create(req.body);
         res.json(course);
     } catch (error) {
-        res.json(error)
+        res.json(error);
     }
 });
 
@@ -49,14 +46,14 @@ router.post("/course/", async (req, res) => {
 router.delete("/course/:courseId", async (req, res) => {
     try {
         await Course.remove({
-            "_id": req.params.courseId
+            _id: req.params.courseId,
         });
         res.status(200).json({
-            "message": "Deleted successfully"
+            message: "Deleted successfully",
         });
     } catch (error) {
         res.json({
-            "message": error
+            message: error,
         });
     }
 });
@@ -64,30 +61,30 @@ router.delete("/course/:courseId", async (req, res) => {
 // Get by title
 
 router.get("/course/title/:title", async (req, res) => {
-
     const title = req.params.title;
     console.log(title);
     try {
-        const course = await Course.find({title:title});
+        const course = await Course.find({ title: title });
         res.json(course);
-
     } catch (err) {
         res.json(err);
     }
 });
 
 // Update course by id
-router.put("/:courseId", async(req, res) => {
+router.put("/:courseId", async (req, res) => {
     const courseId = req.params.courseId;
 
     try {
-        const course = await Course.updateOne({
-            '_id': courseId
-        }, req.body);
+        const course = await Course.updateOne(
+            {
+                _id: courseId,
+            },
+            req.body
+        );
     } catch (error) {
         res.json(error);
     }
-
-})
+});
 
 module.exports = router;
